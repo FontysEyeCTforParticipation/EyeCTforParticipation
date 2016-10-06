6f55cea25795c51383cf3dce7ff9c32e115fcaa7
Sjors van den Besselaar <s.vandenbesselaar@student.fontys.nl>
2016-10-06 13:22:26 +0200

    Admin class added.

---
+++ EyeCTforParticipation/EyeCTforParticipation/Beheerder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeCTforParticipation
+{
+    class Admin : User
+    {
+        //constructor
+        public Admin(string Name, string Email, string Password, int RFID, DateTime DateOfBirth) : base(Name, Email, Password, RFID, DateOfBirth)
+        {
+
+        }
+    }
+}
