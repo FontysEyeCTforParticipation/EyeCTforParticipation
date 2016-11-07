59587d3d62dbbc7dcc7515ec714d75dd43ad56bf
WhiteBlade21 <Jens.van.meijel@gmail.com>
2016-11-07 10:51:16 +0100

    Implemented ChatSQLContext Listmethods

--- EyeCTforParticipation/EyeCTforParticipation/Data/ChatSQLContext.cs
+++ EyeCTforParticipation/EyeCTforParticipation/Data/ChatSQLContext.cs
@@ -18,9 +18,119 @@ namespace EyeCTforParticipation.Data
             throw new NotImplementedException();
         }
 
-        public List<ChatModel> List(int userid)
-        { 
-            throw new NotImplementedException();
+        public List<ChatModel> List(int userId)
+        {
+            List<ChatModel> result = null;
+            string query = @"SELECT Application.Id as Id, HelpRequest.Title as Title, Application.Status as Status 
+                             FROM Application 
+                             JOIN HelpRequest ON HelpRequest.Id = Application.HelpRequestId;"; 
+                             
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(new ChatModel
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Status = (ApplicationStatus)reader.GetInt32(2)
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<ChatModel> ListAsAidWorkerUser(int userId)
+        {
+            List<ChatModel> result = null;
+            string query = @"SELECT Application.Id as Id, HelpRequest.Title as Title, Application.Status as Status 
+                             FROM Application 
+                             JOIN HelpRequest ON HelpRequest.Id = Application.HelpRequestId;
+                             JOIN HelpSeekerAidWorker ON HelpRequest.HelpSeekerUserId = HelpSeekerAidWorker.HelpSeekerUserId 
+                             Where AidWorkerUserId = @AidWorkerUserId;";
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                cmd.Parameters.AddWithValue("@AidWorkerUserId", userId);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(new ChatModel
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Status = (ApplicationStatus)reader.GetInt32(2)
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<ChatModel> ListAsHelpSeeker(int userId)
+        {
+            List<ChatModel> result = null;
+            string query = @"SELECT Application.Id as Id, HelpRequest.Title as Title, Application.Status as Status 
+                             FROM Application 
+                             JOIN HelpRequest ON HelpRequest.Id = Application.HelpRequestId 
+                             Where HelpSeekerUserId = @HelpSeekerUserId;";
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                cmd.Parameters.AddWithValue("@HelpSeekerUserId", userId);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(new ChatModel
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Status = (ApplicationStatus)reader.GetInt32(2)
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<ChatModel> ListAsVolunteer(int userId)
+        {
+            List<ChatModel> result = null;
+            string query = @"SELECT Application.Id as Id, HelpRequest.Title as Title, Application.Status as Status 
+                             FROM Application 
+                             JOIN HelpRequest ON HelpRequest.Id = Application.HelpRequestId 
+                             Where VolunteerId = @VolunteerId;";
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                cmd.Parameters.AddWithValue("@VolunteerId", userId);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(new ChatModel
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Status = (ApplicationStatus)reader.GetInt32(2)
+                        });
+                    }
+                }
+            }
+            return result;
         }
 
         public void SendMessage(MessageModel message)
